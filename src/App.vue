2ce6636065fe78fba5789a36d7f35895a62a9b8e
Appz-dk <patrick.fyhn@gmail.com>
2023-10-29 14:49:17 +0100

    setup html & styles

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,79 @@
 </script>
 
 <template>
+  <div class="container">
+    <header>
+      <h1>Quizes</h1>
+      <input type="text" placeholder="Search..."/>
+    </header>
+    <main>
+      <div class="quiz-container">
+        <div class="quiz-card">
+          <img src="https://i.ytimg.com/vi/Kp2bYWRQylk/maxresdefault.jpg"/>
+          <div class="card-text">
+            <h2>Math</h2>
+            <p>15 Questions</p>
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
 </template>
 
-<style scoped>
+<!-- Have to add lang="postcss" for no errors even though native css supports nesting -->
+<style lang="postcss" scoped>
+  .container {
+    max-width: 1000px;
+    margin: 0 auto;
+    padding-top: 4rem
+  }
+
+  header {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+
+    & input {
+      border-radius: 8px;
+      padding: 8px;
+      background-color: #eee;
+      border: none;
+    }
+  }
+
+  .quiz-container {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    place-items: center;    
+    margin-top: 2rem;
+  }
+
+  /* Card styling */
+
+  .quiz-card {
+    width: 300px;
+    overflow: hidden;
+    border-radius: 8px;
+    box-shadow: 1px 1px 10px rgba(0,0,0,0.1);
+    cursor: pointer;
+
+    & img {
+      display: block;
+      width: 100%;
+      height: 200px;
+      margin: 0;
+    }
+
+    & .card-text {
+      padding: .5rem 1rem
+    }
+
+    & p, h2 {
+      margin-block: .5rem;
+    }
+  }
+
+
+  
+
 </style>
