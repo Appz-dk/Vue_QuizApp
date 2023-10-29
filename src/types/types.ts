fea17ae89d341b14c2ebfc272853b0fbb60adf72
Appz-dk <patrick.fyhn@gmail.com>
2023-10-29 23:09:22 +0100

    Updated types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,22 @@
-import q from "../assets/data.json"
+// import q from "../assets/data.json"
+// export type TQuiz = typeof q[0]
 
-export type TQuiz = typeof q[0]
+export type TQuiz = {
+  id: number;
+  img: string;
+  name: string;
+  questions: TQuestion[]
+}
+
+export type TQuestionOption = {
+  id: number;
+  label: string;
+  text: string;
+  isCorrect: boolean
+}
+
+export type TQuestion = {
+  id: number;
+  text: string;
+  options: TQuestionOption[]
+}
