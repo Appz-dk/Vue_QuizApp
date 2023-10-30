67a9b363650013a79f0a8f8c3ffee57ef0fa8d3a
Appz-dk <patrick.fyhn@gmail.com>
2023-10-30 11:01:39 +0100

    Added QuizResults & routing

---
+++ src/components/QuizResult.vue
@@ -0,0 +1,55 @@
+<script lang="ts" setup>
+  import { useRouter } from "vue-router"
+
+  const props = defineProps({
+    result: {
+      required: true,
+      type: String
+    }
+  })
+
+  const router = useRouter()
+  const navigateToQuizes = () => {
+    router.push("/")
+  }
+</script>
+
+<template>
+  <div class="results">
+    <h1>Quiz over!</h1>
+    <p>{{ props.result }}</p>
+    <button @click="navigateToQuizes">Back to quizes</button>
+  </div>
+</template>
+
+<style scoped>
+  .results {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+    text-align: center;
+  }
+
+  h1 {
+    margin-bottom: 1rem;
+  }
+  
+  p {
+    font-size: 1.5rem;
+    margin-bottom: 1rem;
+  }
+
+  button {
+    cursor: pointer;
+    padding: .25rem .75rem;
+    border-radius: 8px;
+    border: none;
+    background-color: bisque;
+    font-size: 1rem;
+  }
+
+  button:hover {
+    outline: 1px solid black;
+  }
+</style>
--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -2,6 +2,7 @@
   import { useRoute } from "vue-router";
   import Question from "@/components/Question.vue"
   import QuizHeader from "@/components/QuizHeader.vue"
+  import QuizResult from "@/components/QuizResult.vue"
   import quizData from "@/assets/data.json"
   import { ref, computed } from "vue";
 
@@ -9,27 +10,35 @@
   const quizId = route.params.quizId as string
 
   const currentQuestionIndex = ref(0)
+  // TODO: Maybe show error if no quiz is found
   const quiz = quizData.find(quiz => quiz.id === parseInt(quizId))
   const currentQuestion = computed(() => quiz?.questions[currentQuestionIndex.value])
-  const quizState = computed(() => `${currentQuestionIndex.value + 1}/${quiz?.questions.length}`)
-  const completionProcent = computed(() => (currentQuestionIndex.value / (quiz?.questions?.length || 1)) * 100)
+  const quizLength = computed(() => quiz?.questions.length || 0)
+  const quizState = computed(() => `${currentQuestionIndex.value + 1}/${quizLength.value}`)
+  const completionProcent = computed(() => (currentQuestionIndex.value / quizLength.value) * 100)
+  const quizOver = computed(() => currentQuestionIndex.value >= quizLength.value)
   const correctAnswers = ref(0)
 
   const optionSelectedHandler = (isCorrect: boolean) => {
     if (isCorrect) {
       correctAnswers.value += 1
     }
-    currentQuestionIndex.value += 1
+
+      currentQuestionIndex.value += 1
+  }
+
+  const getQuizResult = () => {
+    return `You got ${correctAnswers.value} out of ${quizLength.value} questions correct`
   }
 
 </script>
 
 <template>
-  <div v-if="quiz" class="quiz-container">
+  <div class="quiz-container">
     <QuizHeader :quizState="quizState" :completionProcent="completionProcent"/>
-    <Question v-if="currentQuestion" :question="currentQuestion" @selectOption="optionSelectedHandler"/>
+    <Question v-if="currentQuestion && !quizOver" :question="currentQuestion" @selectOption="optionSelectedHandler"/>
+    <QuizResult v-else :result="getQuizResult()" />
   </div>
-  <button @click="currentQuestionIndex++">Next Question</button>
 </template>
 
 
